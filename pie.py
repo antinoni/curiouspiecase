0dc19b6c65750ef09de784290353944410fc92ce
antinoni <ankakouri@gmail.com>
2020-12-13 13:16:08 +0200

    update pie.py

--- pie.py
+++ pie.py
@@ -1,4 +1,4 @@
-# import random
+import random
 import os
 # import keyboard
 import pygame
@@ -11,7 +11,6 @@ img_folder = os.path.join(game_folder, "images")
 WIDTH = 800
 HEIGHT = 600
 FPS = 30
-transparent = (0, 0, 0, 0)
 
 ########################################### PLAYER CLASS ###########################################
 
@@ -39,7 +38,6 @@ class Player(pygame.sprite.Sprite):
             os.path.join(img_folder, "mf3.png"))]
         self.curr_sprite = 0
         self.image = self.down[self.curr_sprite]
-        self.image.set_colorkey((0, 0, 0))
 
         # movement
         self.rect = self.image.get_rect()
@@ -65,7 +63,7 @@ class Player(pygame.sprite.Sprite):
             if self.curr_sprite > 2:
                 self.curr_sprite = 0
             self.image = self.right[int(self.curr_sprite)]
-        if keystate[pygame.K_UP] and self.rect.top > 370:
+        if keystate[pygame.K_UP] and self.rect.top > 200:
             self.speedy = -5
             self.curr_sprite += 0.4
             if self.curr_sprite > 2:
@@ -80,6 +78,16 @@ class Player(pygame.sprite.Sprite):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
 
+########################################## SUSPECTS CLASS #########################################
+
+
+class Suspects(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = [1, 2, 3, 4, 5, 6]
+        self.names = ['Roger', 'Rose', 'Melanie', 'Mark', 'Eve', 'Elon']
+        self.culprit = random.choice(self.names)
+
 ########################################## OBJECTS CLASSES #########################################
 
 
@@ -90,23 +98,33 @@ class Couch(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH/5, 2*HEIGHT/3)
 
-    def shooter(self):
-        shooterbg = pygame.Surface((300, 100))
-        shooterbg.fill((55, 155, 255))
-        screen.blit(shooterbg, (WIDTH/2-WIDTH/6 - 10, HEIGHT/3))
+########################################## MINIGAMES #########################################
+
+
+def hidden_object():
+    shooterbg = pygame.Surface((600, 400))
+    shooterbg.fill((55, 155, 255))
+    screen.blit(shooterbg, (100, 100))
+
+### RIDDLE ###
 
 
 def riddlegame():
-    # self.image = pygame.image.load(os.path.join(img_folder, "bookpage.png"))
-    shooterbg = pygame.Surface((200, 300))
-    shooterbg.fill((255, 255, 255))
-    screen.blit(shooterbg, (WIDTH/2 - 100, HEIGHT/4))
-    riddle = ["What is seen in the", "middle of March and",
-              "April that can’t be", "    seen at the     ", "beginning or end of", "   either month?   "]
+    book = pygame.image.load(os.path.join(img_folder, "bookpage.png"))
+    screen.blit(book, (WIDTH/2 - 102, HEIGHT/4))
+    if suspects.culprit == 'Roger' or suspects.culprit == 'Rose':
+        riddle = ["What is seen in the", "middle of March and",
+                  "April that can’t be", "    seen at the     ", "beginning or end of", "   either month?   "]
+    elif suspects.culprit == 'Melanie' or suspects.culprit == 'Mark':
+        riddle = ["What appears once", "in a minute, twice",
+                  "in a moment, but", "never in a thousand", "years?", ""]
+    elif suspects.culprit == 'Eve' or suspects.culprit == 'Elon':
+        riddle = [
+            "I am the beginning ", "of eternity,the end", "of time and space.", "The beginning of", "every end, and the", "end of every place."]
     posy = HEIGHT/4 + 30
     for i in range(6):
         riddlesurface = myfont.render(
-            riddle[i], False, (10, 10, 10))
+            riddle[i], False, (65, 10, 10))
         screen.blit(riddlesurface, (WIDTH/2 - 85, posy))
         posy += 20
 
@@ -133,7 +151,7 @@ all_sprites.add(player)
 couch = Couch()
 all_sprites.add(couch)
 objects.add(couch)
-
+suspects = Suspects()
 
 ########################################### GAME LOOP ###########################################
 running = True
@@ -141,17 +159,11 @@ while running:
     # keep loop running at the right speed
     clock.tick(FPS)
 
-    key_pressed = False
     # Process input (events)
     for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
-                key_pressed = True
-            if event.key == pygame.K_ESCAPE:
-                key_pressed = False
 
     # Update
     all_sprites.update()
@@ -160,18 +172,26 @@ while running:
 
     ########################################### BOTTOM MESSAGE ###########################################
 
-    if player.rect.left - couch.rect.right <= 3:
-        text = 'The couch... I see some crumbs here.'
-    elif player.rect.right < 700 and player.rect.right > 510 and player.rect.top < 375:
-        text = 'I don\'t think I should leave an active crime scene.'
-    elif player.rect.right > 700 and player.rect.top > 390 and player.rect.top < 470:
+    keystate = pygame.key.get_pressed()
+    if player.rect.right > 700 and player.rect.top > 220 and player.rect.top < 300:
         text = 'That room is irrelevant. All the action is here.'
-    elif player.rect.right < 390 and player.rect.right > 310 and player.rect.top < 375:
+    elif player.rect.left - couch.rect.right <= 5 and player.rect.top > 205:
+        text = 'The couch... I see some crumbs here.'
+        if keystate[pygame.K_SPACE]:
+            text = 'I have to find:, but only one of them will help my cause.'
+        if keystate[pygame.K_h]:
+            text = 'HINT: Which of these items has something in common with some of our suspects?'
+    elif player.rect.right < 700 and player.rect.right > 510 and player.rect.top < 200:
+        text = 'I don\'t think I should leave an active crime scene.'
+    elif player.rect.right < 420 and player.rect.right > 340 and player.rect.top < 200:
         text = 'An open book... A note written in it... And a stain from what looks like cherry jam.'
+        if keystate[pygame.K_SPACE]:
+            text = 'The answer to the riddle will help me find the culprit.'
+        if keystate[pygame.K_h]:
+            text = 'HINT: Look closely at the given words. Do you notice anything?'
     else:
-        text = 'Use ARROWS to move, SPACE to interact with objects'
-    textsurface = myfont.render(
-        text, False, (255, 255, 255))
+        text = 'Use ARROWS to move, hold SPACE to interact with objects. Hold \'H\' for a hint.'
+    textsurface = myfont.render(text, False, (255, 255, 255))
     text_width, text_height = myfont.size(text)
 
     ########################################### DRAW ###########################################
@@ -181,12 +201,11 @@ while running:
 
     all_sprites.draw(screen)
 
-    # if keyboard.is_pressed('Space'):
     if player.rect.left - couch.rect.right <= 3:
-        couch.shooter()
-    elif player.rect.right < 390 and player.rect.right > 310 and player.rect.top < 375:
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_SPACE]:
+        if keystate[pygame.K_SPACE] or keystate[pygame.K_h]:
+            hidden_object()
+    elif player.rect.right < 420 and player.rect.right > 340 and player.rect.top < 200:
+        if keystate[pygame.K_SPACE] or keystate[pygame.K_h]:
             riddlegame()
 
     pygame.display.update()
